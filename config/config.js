0fd42d4460a9ff959396045553d281a1bd5ccb36
Ng Bryan <ngbryan@Ngs-MacBook-Pro.local>
2021-07-25 01:45:38 +0800

    revert production string for heroku

--- config/config.js
+++ config/config.js
@@ -6,10 +6,8 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
-const prodConnectionString = `postgresql://${process.env.RDS_USERNAME}:${process.env.RDS_PASSWORD}@${process.env.RDS_HOSTNAME}:${process.env.RDS_PORT}/${process.env.RDS_DB_NAME}`;
-
 const pool = new Pool({
-  connectionString: isProduction ? prodConnectionString : connectionString,
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
 });
 
 module.exports = { pool };
